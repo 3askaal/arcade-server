480c5c69cd57c5cd9fe9c0c4c3fe9978fbcef846
Bas Kaal <3askaal@gmail.com>
2021-12-23 20:41:59 +0100

    progress on implementing sockets

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+// import { connect } from 'mongoose';
+import { CONFIG } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(CONFIG.PORT);
+    // await connect(CONFIG.MONGODB_URI);
+  } catch (err) {
+    throw err;
+  }
 }
+
 bootstrap();
